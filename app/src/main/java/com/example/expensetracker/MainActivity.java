749d5b642cf6e033a5f0ba14a99867b827037799
leoxuvancourver <leoguotengxu@gmail.com>
2022-03-08 13:04:24 -0800

    add MainActivity

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] inteval = {"day","month", "year", "pick"};
+    DatePickerDialog datePickerDialog;
+    Calendar calendar;
+    int mYear, mMonth, mDay, mHour, mMinute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setNavigationIcon(R.drawable.search);
+
+        Spinner mySpinner = findViewById(R.id.spinner_toolbar);
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item, inteval);
+        mySpinner.setAdapter(spinnerAdapter);
+
+        calendar=Calendar.getInstance();
+        mYear=calendar.get(Calendar.YEAR);
+        mMonth=calendar.get(Calendar.MONTH);
+        mDay=calendar.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+
+            }
+        },mYear,mMonth,mDay);
+
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==3){
+                    datePickerDialog.show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
