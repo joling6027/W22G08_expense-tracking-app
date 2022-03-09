8b1fb936743c75198aabde632b3ba1bc399095f9
leoxuvancourver <leoguotengxu@gmail.com>
2022-03-09 13:30:09 -0800

    add pics add datapicker

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.expensetracker;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,6 +12,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.DatePickerDialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
@@ -18,11 +21,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.time.Month;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,12 +36,15 @@ public class MainActivity extends AppCompatActivity {
     String[] inteval = {"day","month", "year", "pick"};
     DatePickerDialog datePickerDialog;
     Calendar calendar;
-    int mYear, mMonth, mDay, mHour, mMinute;
+    int mYear, mMonth, mDay;
     DrawerLayout dLayout;
     SearchView simpleSearchView;
+    TextView textViewInterval;
+    Month monthName;
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,14 +72,33 @@ public class MainActivity extends AppCompatActivity {
         mMonth=calendar.get(Calendar.MONTH);
         mDay=calendar.get(Calendar.DAY_OF_MONTH);
 
-        datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+        //textView Interval
+        textViewInterval=findViewById(R.id.txtViewInterval);
+        monthName = Month.of(mMonth+1);
+        textViewInterval.setText(mDay+" "+monthName+" "+mYear);
+
+
+
+        // input expense
+        ImageView imageViewExpense=findViewById(R.id.imageViewExpense);
+
+        imageViewExpense.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
-
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ExpenseActivity.class));
             }
-        },mYear,mMonth,mDay);
+        });
 
+
+        //input income
+        ImageView imageViewIncome=findViewById(R.id.imageViewIncome);
+        imageViewIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,IncomeActivity.class));
+            }
+        });
 
 
     }
@@ -83,17 +111,32 @@ public class MainActivity extends AppCompatActivity {
         // initiate a Navigation View
         // implement setNavigationItemSelectedListener event on NavigationView
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId=item.getItemId();
                 if(itemId==R.id.month){
-                    //
+                    textViewInterval.setText(monthName.toString());
+                    dLayout.closeDrawers();
                 }
                 if(itemId==R.id.year){
-                    //
+                    textViewInterval.setText(mYear+" ");
+                    dLayout.closeDrawers();
                 }
                 if(itemId==R.id.day){
+                    datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                mDay=day;
+                                mMonth=month;
+                                mYear=year;
+                                monthName = Month.of(mMonth+1);
+                                textViewInterval.setText(mDay+" "+monthName+" "+mYear);
+                        }
+                    },mYear,mMonth,mDay);
                     datePickerDialog.show();
+
+
                     dLayout.closeDrawers();
                 }
 
@@ -120,6 +163,10 @@ public class MainActivity extends AppCompatActivity {
                 searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
+
+
+
+    //the end
 }
 
 
